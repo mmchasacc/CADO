8ca4116de2e5d64de914fd04bedf3e44e25e8a39
mmchasacc <mehdi.mirzaie@chasacademy.se>
2025-12-30 12:54:14 +0100

    last commit was in the wrong directory, gitignore modified, .env should not be visible anymore

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import LoginPage from './pages/LogInPage'
 import SignUpPage from './pages/SignupPage'
 import TodoPage from './pages/TodoPage'
 import { useAuth } from './context/AuthContext'
+import PublicRoute from './routes/PublicRoute'
+import ProtectedRoute from './routes/ProtectedRoute'
 
 function App() {
 
   const {isAuthenticated} = useAuth()
 
   return (
-      isAuthenticated ? <TodoPage /> : <LoginPage />
+      isAuthenticated ? <ProtectedRoute /> : <PublicRoute />
   )
 }
 
--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,13 +1,26 @@
 import { useContext, useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const { login } = useAuth()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  const navigate = useNavigate()
 
+  const handleLogin = () => {
+    const savedData = localStorage.getItem("user_pass")
 
+    const user = JSON.parse(savedData)
+
+    if (user.email === email && user.password === password) {
+      navigate("/todo")
+    } else {
+      alert("Wrong email or password")
+    }
+  }
+  
   return (
     <>
       <div className="flex flex-col gap-8">
@@ -26,11 +39,11 @@ const LoginPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button>Log in</button>
+        <button type="submit" onClick={handleLogin} className="bg-amber-300 hover:bg-amber-600 active:bg-amber-100 text-black">Log in</button>
       </div>
-      <button>Sign up</button>
+      <button type="submit">Sign up</button>
     </>
   );
-};
+}
 
 export default LoginPage;
